af0da434de7809a8624b915410ebf011105d3499
Vlad Shestavetkyi <95980041+proydusvit@users.noreply.github.com>
2023-11-09 19:27:00 +0200

    add

---
+++ src/api/foto-colection/routes/foto-colection.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * foto-colection router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::foto-colection.foto-colection');
---
+++ src/api/link-photo/controllers/link-photo.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * link-photo controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::link-photo.link-photo');
