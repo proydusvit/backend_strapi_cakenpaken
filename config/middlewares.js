fd0002a269e335b36e8e02d7cc97b7c29eef53bb
Vlad Shestavetkyi <95980041+proydusvit@users.noreply.github.com>
2023-11-10 17:09:51 +0200

    add

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,26 +1,14 @@
 module.exports = [
   "strapi::errors",
   {
-    name: "strapi::security",
+    name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "market-assets.strapi.io",
-            "res.cloudinary.com",
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "market-assets.strapi.io",
-            "res.cloudinary.com",
-          ],
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,
         },
       },
